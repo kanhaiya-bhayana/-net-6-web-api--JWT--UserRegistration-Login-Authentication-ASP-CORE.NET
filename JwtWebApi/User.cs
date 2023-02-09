e83b1b5279d5c8c7a55521a9a253c79f2f57600f
Kanhaiya <kbhayana60_be20@thapar.edu>
2023-02-09 13:28:53 +0530

    initial fixed Jwt-User registeration/login/authentication

---
+++ JwtWebApi/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtWebApi
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set;}
+    }
+}
