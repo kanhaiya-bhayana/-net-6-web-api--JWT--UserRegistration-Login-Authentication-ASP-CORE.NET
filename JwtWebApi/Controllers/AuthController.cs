96767334b735fa3ef2c1e2643cf7187d86b68f09
Kanhaiya <kbhayana60_be20@thapar.edu>
2023-02-13 00:52:53 +0530

    Added the functionality of Refresh-token.

--- JwtWebApi/Controllers/AuthController.cs
+++ JwtWebApi/Controllers/AuthController.cs
@@ -63,7 +63,55 @@ namespace JwtWebApi.Controllers
                 return BadRequest("Wrong password.");
             }
             string token = CreateToken(user);
+
+            var refreshToken = GenerateRefreshToken();
+            SetRefreshToken(refreshToken);
             return Ok(token);
+        }
+
+
+        [HttpPost("refresh-token")]
+        public async Task<ActionResult<string>> RefreshToken()
+        {
+            var refreshToken = Request.Cookies["refreshToken"];
+
+            if (!user.RefreshToken.Equals(refreshToken))
+            {
+                return Unauthorized("Invalid Refresh Token.");
+            }
+            else if (user.TokenExpires < DateTime.Now)
+            {
+                return Unauthorized("Token expired.");
+            }
+            string token = CreateToken(user);
+            var newRefreshToken = GenerateRefreshToken();
+            SetRefreshToken(newRefreshToken);
+            return Ok(token);
+        }
+
+        private RefreshToken GenerateRefreshToken()
+        {
+            var refreshToken = new RefreshToken
+            {
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                Expires = DateTime.Now.AddDays(7),
+                Created = DateTime.Now
+            };
+            return refreshToken;
+        }
+
+        private void SetRefreshToken(RefreshToken newRefreshToken)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly= true,
+                Expires = newRefreshToken.Expires
+            };
+            Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+
+            user.RefreshToken = newRefreshToken.Token;
+            user.TokenCreated = newRefreshToken.Created;
+            user.TokenExpires= newRefreshToken.Expires;
         }
 
         private string CreateToken(User user)
@@ -88,8 +136,6 @@ namespace JwtWebApi.Controllers
 
             return jwt;
         }
-
-
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
---
+++ JwtWebApi/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtWebApi
+{
+    public class RefreshToken
+    {
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime Expires { get; set; }
+    }
+}
